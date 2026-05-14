0edd8052c071e1b4451208799b0afa421fd547fc
Iovu Rares <raresciovu@gmail.com>
2026-05-14 14:20:06 +0300

    Updated frontend and fixed log clear route

--- routes/dashboard.py
+++ routes/dashboard.py
@@ -6,8 +6,9 @@ dashboard_bp = Blueprint('dashboard', __name__)
 @dashboard_bp.route('/clear-logs', methods=['POST'])
 def clear_logs():
     try:
-        print("DELETED")
-        with open('db_log.json', 'w') as f:
+        with open('data/db_log.json', 'w') as f:
+            json.dump([], f)
+        with open('data/ai_insights.json', 'w') as f:
             json.dump([], f)
         return {"status": "success", "message": "Logs cleared"}, 200
     except Exception as e:
