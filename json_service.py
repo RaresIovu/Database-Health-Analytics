6a4b99d88dbb9a9ffabc7e385e86c38d22818e88
Iovu Rares <raresciovu@gmail.com>
2026-05-14 13:39:17 +0300

    Moved metrics to data directory

--- json_service.py
+++ json_service.py
@@ -1,21 +1,25 @@
+import os
 import json
 from datetime import datetime
 
 def save_to_json(new_data, filename):
     new_data["timestamp"] = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     try:
-        with open(filename, "r") as file:
+        DATA_DIR = "data"
+        if not os.path.exists(DATA_DIR):
+            os.makedirs(DATA_DIR)
+        with open(f"data/{filename}", "r") as file:
             data_list = json.load(file)
     except (FileNotFoundError, json.JSONDecodeError):
         data_list = []
     data_list.append(new_data)
 
-    with open(filename, "w") as file:
+    with open(f"data/{filename}", "w") as file:
         json.dump(data_list, file, indent=4)
 
 def get_all_logs(filename):
     try:
-        with open(filename, "r") as file:
+        with open(f"data/{filename}", "r") as file:
             return json.load(file)
     except (FileNotFoundError, json.JSONDecodeError):
         return []
